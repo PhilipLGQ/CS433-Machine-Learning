cf8256146c1d568ef9df8528dd65fe7e5f89b6b9
PhilipLGQ <guanqun.liu@epfl.ch>
2021-10-07 16:26:51 +0200

    project files added

---
+++ projects/project1/scripts/implementations.py
@@ -0,0 +1,42 @@
+# implementation.py
+
+# This file applies all required ML methods in Project1
+
+# Included methods:
+# least_squares_GD, least_squares_SGD, least_squares, ridge_regression,
+# logistic_regression, and reg_logistic_regression
+
+
+from proj1_helpers import *
+import numpy as np
+
+def least_squares_GD(y, tx, initial_w, max_iters, gamma):
+
+    return w, loss
+
+
+def least_squares_SGD(y, tx, initial_w, max_iters, gamma):
+
+    return w, loss
+
+
+def least_squares(y, tx):
+
+    return w, loss
+
+
+def ridge_regression(y, tx, lambda_):
+
+    return w, loss
+
+
+def logistic_regression(y, tx, initial_w, max_iters, gamma):
+
+    return w, loss
+
+
+def reg_logistic_regression(y, tx, lambda_, initial_w, max_iters, gamma):
+
+    return w, loss
+
+
